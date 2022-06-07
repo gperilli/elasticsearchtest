51f4c65630b7a6fb62bc4709567dc01b264df48d
Gareth Perilli <garethperilli@gmail.com>
2022-06-07 16:33:36 +0900

    code clean up

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,8 @@
 class MoviesController < ApplicationController
     def index
-        #@movies = Movie.all
-        #raise
         search = params[:query].present? ? params[:query] : nil
         @movies = if search
             Movie.search(search)
-            #@movies = Movie.search("apples")
         else
             Movie.all
         end
